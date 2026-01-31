862e60d63ad4ab0f22116da35ed8f79277579318
AndradeTC86 <andrade-tc@uol.com.br>
2026-01-31 08:25:16 -0300

    Estrategias de locators com playwright

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -7,16 +7,18 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     /// Arrange 
     await page.goto('http://localhost:5173/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-    /// Act
-    await page.getByTestId('search-order-id').fill('VLO-JZURBQ')
+    /// Act    
+    await page.getByLabel('Número do Pedido').fill('VLO-JZURBQ')    
     await page.getByTestId('search-order-button').click()
 
     /// Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible()
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-JZURBQ')
+
     await expect(page.getByTestId('order-result-status')).toBeVisible()
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
