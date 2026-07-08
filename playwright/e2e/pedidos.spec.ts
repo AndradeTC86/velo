e341ef40024fb358174cf7b81631c1ebbe1eae89
AndradeTC86 <90931093+AndradeTC86@users.noreply.github.com>
2026-07-08 19:17:07 -0300

    test: removendo massa de dados fixa no codigo por uma variavel

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,20 +4,23 @@ import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
     
-    /// Arrange 
+    // Test Data
+    const order = 'VLO-JZURBQ'
+    
+    // Arrange 
     await page.goto('http://localhost:5173/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-    /// Act    
-    await page.getByLabel('Número do Pedido').fill('VLO-JZURBQ')    
+    // Act    
+    await page.getByLabel('Número do Pedido').fill(order)    
     await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
-    /// Assert
+    // Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
-    await expect(page.getByTestId('order-result-id')).toContainText('VLO-JZURBQ')
+    await expect(page.getByTestId('order-result-id')).toContainText(order)
 
     await expect(page.getByTestId('order-result-status')).toBeVisible()
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
