1b322cad5d989aff0c07d0583db2fba96cc91f80
AndradeTC86 <90931093+AndradeTC86@users.noreply.github.com>
2026-07-13 09:49:44 -0300

    refactor: criando hooks para reaproveitamento de código

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,50 +3,47 @@ import { generateOrderCode } from '../support/helpers'
 
 /// AAA - Arrange, Act, Assert
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
+test.describe('Consulta de Pedido', async () => {
+
+    test.beforeEach(async ({ page }) => {
+        // Arrange 
+        await page.goto('http://localhost:5173/')
+        await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+        await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+        await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+    })
+
+    test('deve consultar um pedido aprovado', async ({ page }) => {
+        // Test Data
+        const order = 'VLO-JZURBQ'
     
-    // Test Data
-    const order = 'VLO-JZURBQ'
-    
-    // Arrange 
-    await page.goto('http://localhost:5173/')
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+        // Act    
+        await page.getByLabel('Número do Pedido').fill(order)    
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+        // Assert
+        await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
+        await expect(page.getByTestId('order-result-id')).toContainText(order)
 
-    // Act    
-    await page.getByLabel('Número do Pedido').fill(order)    
-    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+        await expect(page.getByTestId('order-result-status')).toBeVisible()
+        await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+    })
 
-    // Assert
-    await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
-    await expect(page.getByTestId('order-result-id')).toContainText(order)
+    test('deve exibir mensagem quando o pedido não é encontrado', async ({ page })=> {
+        // Test Data
+        const order = generateOrderCode()    
+        
+        // Act    
+        await page.getByLabel('Número do Pedido').fill(order)    
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
-    await expect(page.getByTestId('order-result-status')).toBeVisible()
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+        // Assert
+        await expect(page.locator('#root')).toMatchAriaSnapshot(`
+        - img
+        - heading "Pedido não encontrado" [level=3]
+        - paragraph: Verifique o número do pedido e tente novamente
+        `)
+    })
 })
 
-test('deve exibir mensagem quando o pedido não é encontrado', async ({ page })=> {
-
-    // Test Data
-    const order = generateOrderCode()
-    
-    // Arrange 
-    await page.goto('http://localhost:5173/')
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-    // Act    
-    await page.getByLabel('Número do Pedido').fill(order)    
-    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-
-    // Assert
-    await expect(page.locator('#root')).toMatchAriaSnapshot(`
-    - img
-    - heading "Pedido não encontrado" [level=3]
-    - paragraph: Verifique o número do pedido e tente novamente
-    `)
-})
