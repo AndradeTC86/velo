e9646f1ca7faefc704e9d682def99ec4de9f1639
AndradeTC86 <90931093+AndradeTC86@users.noreply.github.com>
2026-07-13 10:05:59 -0300

    refactor: alterando assercao do teste para utilizar validação visual através de aria snapshot

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -23,11 +23,34 @@ test.describe('Consulta de Pedido', async () => {
         await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
         // Assert
-        await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
-        await expect(page.getByTestId('order-result-id')).toContainText(order)
-
-        await expect(page.getByTestId('order-result-status')).toBeVisible()
-        await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+        await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order}
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Midnight Black
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: aero Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Motorista de Fuga
+            - paragraph: Email
+            - paragraph: driver@mailinator.com
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+        `)
     })
 
     test('deve exibir mensagem quando o pedido não é encontrado', async ({ page })=> {
