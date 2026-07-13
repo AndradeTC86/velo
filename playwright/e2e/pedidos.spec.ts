5902cd3910686e6155c84874208b30c58f56aad7
AndradeTC86 <90931093+AndradeTC86@users.noreply.github.com>
2026-07-13 09:37:04 -0300

    refactor: modificando teste para gerar codigo de pedido aleatorio

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { generateOrderCode } from '../support/helpers'
 
 /// AAA - Arrange, Act, Assert
 
@@ -29,7 +30,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
 test('deve exibir mensagem quando o pedido não é encontrado', async ({ page })=> {
 
     // Test Data
-    const order = 'VLO-EXEMPLO'
+    const order = generateOrderCode()
     
     // Arrange 
     await page.goto('http://localhost:5173/')
---
+++ playwright/support/helpers.ts
@@ -0,0 +1,9 @@
+export function generateOrderCode() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let suffix = ''
+    
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length)
+        suffix += chars[randomIndex]
+    }
+    return `VLO-${suffix}`}
